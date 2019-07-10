ec4f6b187d34901e82e810949355eef1c249fe4a
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-07-10 14:08:23 -0700

    Working on cosmosdb crud operations

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -1,10 +1,16 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { CosmosSqlHelper } from "../Dal/CosmosSqlHelper";
+import { CosmosClient, SqlQuerySpec, Items } from "@azure/cosmos";
+import { Person } from "../Dto/person";
+
+let helper: CosmosSqlHelper;
+let client: CosmosClient;
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const helper = new CosmosSqlHelper();
-    const client = await helper.getClient();
+    helper = new CosmosSqlHelper();
+    client = await helper.getClient();
 
     const id = req.params ? req.params.id : undefined;
     
@@ -45,27 +51,92 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
 const getOne = async (id: any): Promise<any> => {
     console.log('gettingOne');
-    console.log('gettingOne');
+    const querySpec: SqlQuerySpec = {
+        query: "SELECT VALUE r.people FROM root r WHERE r.id = @id",
+        parameters: [
+            {
+                name: "@id",
+                value: `${id}`
+            }
+        ]
+    };
+
+    const { result: results } = await client
+        .database(helper.databaseId)
+        .container(helper.containerId)
+        .items
+        .query(querySpec, { enableCrossPartitionQuery: true }).toArray();
+
+    console.log(JSON.stringify(results, null, 2));
+
     return id;
 }
 
 const getMany = async (req: any): Promise<any> => {
     console.log('gettingMany');
+    const querySpec: SqlQuerySpec = {
+        query: "",
+        parameters: [
+            {
+                name: "",
+                value: ""
+            }
+        ]
+    };
+
+    const { result: results } = await client
+        .database(helper.databaseId)
+        .container(helper.containerId)
+        .items
+        .query(querySpec, { enableCrossPartitionQuery: true }).toArray();
+     
+    console.log(JSON.stringify(results, null, 2));
+     
     return req;
 }
 
 const insertOne = async (req, id): Promise<any> => {
     console.log('insertingOne');
+    let person = new Person();
+    person.firstName = req.body.firstName;
+    person.lastName = req.body.lastName;
+    const item = await client
+        .database(helper.containerId)
+        .container(helper.containerId)
+        .items
+        .upsert(person);
+
+    console.log(JSON.stringify(item, null, 2));
+    
     return id;
 }
 
 const updateOne = async (req, id): Promise<any> => {
     console.log('updatingOne');
+    let person = new Person();
+    person.firstName = req.body.firstName;
+    person.lastName = req.body.lastName;
+    const { item } = await client
+        .database(helper.containerId)
+        .container(helper.containerId)
+        .item(id)
+        .replace(person);
+
+    console.log(JSON.stringify(item, null, 2));
+    
     return req;
 }
 
 const deleteOne = async (id): Promise<any> => {
     console.log('deleteOne');
+    const { item } = await client
+        .database(helper.databaseId)
+        .container(helper.containerId)
+        .item(id)
+        .delete();
+
+    console.log(JSON.stringify(item, null, 2));    
+    
     return id;
 }
 
