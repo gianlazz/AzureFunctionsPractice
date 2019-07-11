bf6cae13832815ea2bb83b9381b8881fdec2fcc7
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-07-11 14:15:25 -0700

    Stubbing initial test suit

---
+++ tests/httpTrigger.test.ts
@@ -0,0 +1,48 @@
+beforeAll(async () => {
+
+});
+afterAll(async () => {
+
+});
+
+describe("HttpTrigger", () => {
+    it("POST", async () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+    });
+
+    it("GET", async () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+    });
+
+    it("GET many", async () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+    });
+
+    it("DELETE", async () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+    });
+
+    it("PATCH", async () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+    });
+})
