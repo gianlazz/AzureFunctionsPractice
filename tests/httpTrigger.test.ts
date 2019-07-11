c2ce4e8ffdeb85f6f8f9e7506619b5dc041e1a0f
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-07-11 14:55:59 -0700

    Setup ts-jest launch/debuggin and added first passing POST test case

--- tests/httpTrigger.test.ts
+++ tests/httpTrigger.test.ts
@@ -1,3 +1,6 @@
+import { Person } from "../common/dto/person";
+import Axios from "axios";
+
 beforeAll(async () => {
 
 });
@@ -8,10 +11,16 @@ afterAll(async () => {
 describe("HttpTrigger", () => {
     it("POST", async () => {
         // Arrange
-
+        let person = new Person();
+        person.firstName = "John";
+        person.lastName = "Doe";
+        console.log("Running POST/create against REST.");
         // Act
-
+        const response = await Axios.post('http://localhost:7071/api/HttpTrigger', person);
+        const insertedPersonId = response.data;
         // Assert
+        console.log(`DB Id of newly posted/inserted person ${insertedPersonId}`);
+        expect(response).not.toBeNull();
     });
 
     it("GET", async () => {
